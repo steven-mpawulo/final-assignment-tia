cab7c532ec55080bcb5a00ade70b59104abe5629
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 11:04:26 +0300

    adds search movie by name controller

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -2,12 +2,14 @@ const express = require('express');
 const addMovie = require('../controllers/addMovie');
 const removeMovie = require('../controllers/removeMovie');
 const getMovies = require('../controllers/getMovies');
+const searchMovieByName = require('../controllers/searchMovieByName');
 
 const movieRoute = express.Router();
 
 // route to add a movie
 movieRoute.post('/v1/movies', addMovie);
 movieRoute.get('/v1/movies', getMovies);
+movieRoute.get('/v1/movies/search', searchMovieByName);
 movieRoute.delete('/v1/movies/:movieId', removeMovie);
 
 module.exports = movieRoute;
