65b027fb1f575879dce41c096478147e6de27961
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 10:50:05 +0300

    works on get all movies controller

---
+++ controllers/getMovies.js
@@ -0,0 +1,23 @@
+const con = require("../db/db");
+
+// controller to fetch all movies
+
+const getMovies = (req, res) => {
+    const sql = "SELECT * FROM movies";
+    con.query(sql, function (err, result){
+        if (err) {
+            console.log(err);
+            res.status(400).json({"message": "something went wrong"});
+        } else {
+            console.log(result);
+            if (result) {
+                res.status(200).json({"message": "movies retrieved","movies": result});
+            } else {
+                res.status(404).json({"message": "no movies found"});
+            }
+        }
+    });
+
+}
+
+module.exports = getMovies;
