4b59b9d8054c4fc1da184eeab6d1be813c0e4468
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 10:31:32 +0300

    works on remove movie controller

---
+++ controllers/removeMovie.js
@@ -0,0 +1,24 @@
+const con = require("../db/db");
+
+// controller to remove movie
+const removeMovie = (req, res) => {
+   const movieId = pasreInt(req.params.movieId);
+   console.log(movieId);
+
+   if (movieId) {
+    const sql = "DELETE FROM movies WHERE id = ?";
+    con.query(sql, [id], function(err, result){
+        if (err) {
+            console.log(err);
+            res.status(400).json({"message": "something went wrong"});
+        } else {
+            console.log(result);
+            res.status(200).json({"message": "movie deleted successfully"});
+        }
+    });
+   } else {
+    res.status(400).json({"message": "please provide a valid movie Id"});
+   }
+}
+
+module.exports = removeMovie;
