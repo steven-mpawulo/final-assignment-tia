ad7e1e04ab6d1770ffb5ffbd2c321c9116021396
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 10:08:35 +0300

    creates route to add movies

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const addMovie = require('../controllers/addMovie');
 
 const movieRoute = express.Router();
+
+// route to add a movie
+movieRoute.post('/v1/movies', addMovie);
 
 module.exports = movieRoute;
