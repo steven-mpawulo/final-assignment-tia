ee2eff0f21acc3b107ee0878b45c582e4f141560
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 12:00:32 +0300

    adds comments to routes

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -9,9 +9,13 @@ const movieRoute = express.Router();
 
 // route to add a movie
 movieRoute.post('/v1/movies', addMovie);
+// route to get all movies
 movieRoute.get('/v1/movies', getMovies);
+// route to search movies by name
 movieRoute.get('/v1/movies/search', searchMovieByName);
+// route to sort movies
 movieRoute.get('/v1/movies/sort', sortMovies);
+// route to delete movie
 movieRoute.delete('/v1/movies/:movieId', removeMovie);
 
 module.exports = movieRoute;
