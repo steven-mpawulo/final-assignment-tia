50ea672c614836c27ba4c89151c51b31c5725303
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 10:51:32 +0300

    adds get movies controller to movie route

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const addMovie = require('../controllers/addMovie');
 const removeMovie = require('../controllers/removeMovie');
+const getMovies = require('../controllers/getMovies');
 
 const movieRoute = express.Router();
 
 // route to add a movie
 movieRoute.post('/v1/movies', addMovie);
+movieRoute.get('/v1/movies', getMovies);
 movieRoute.delete('/v1/movies/:movieId', removeMovie);
 
 module.exports = movieRoute;
