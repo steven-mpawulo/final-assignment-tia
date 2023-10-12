a234f8e69c0fa48e0a9804acc7677917689c0e9d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-12 09:54:22 +0300

    adds middleware for json and movie route

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const movieRoute = require('./routes/movieRoute');
 const app = express();
 
 const port = process.env.PORT || 8001;
+
+app.use(express.json());
+app.use('/api/', movieRoute);
 
 app.get('/', (req, res) => {
     res.json({"message": "welcome to my server"});
