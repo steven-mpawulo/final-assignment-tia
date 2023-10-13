711c73ca61fd108969136fdac1801749a32455b5
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-13 11:26:46 +0300

    works on fetching movies from server and rendering them on the front end

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const addMovie = () => {
+const addMovie = async () => {
     let name = document.getElementById("name");
     let genre = document.getElementById("genre");
     let plot = document.getElementById("plot");
@@ -7,14 +7,14 @@ const addMovie = () => {
     let notes = document.getElementById('notes');
 
     if (genre.value && plot.value && releaseDate.value && rating.value && notes.value) {
-        axios.post('http://localhost:8000/api/v1/movies', {
+        await axios.post('http://localhost:8000/api/v1/movies', {
             "name": name,
             "genre": genre,
             "plot": plot,
             "release_date": releaseDate,
             "rating": rating,
             "notes": notes
-        }).then((response) => {
+        }).then(function (response) {
             console.log(response);
             name.value = "";
             genre.value = "";
@@ -24,11 +24,30 @@ const addMovie = () => {
             notes.value = "";
              alert("movie added successfully");
 
-        }).catch((err) => {
+        }).catch(function (err) {
             console.log(err);
             alert("failed to add movie");
         });
     } else {
         alert("please provide the required movie details");
     }
+}
+
+const getMovies = async () => {
+    let moviesList = document.getElementById('movies');
+
+     await axios.get("http://localhost:8000/api/v1/movies").then(function(response) {
+        // console.log(response.data);
+        const movies = response.data.movies;
+        movies.forEach((movie) => {
+            console.log(movie);
+            let li = document.createElement('li');
+            li.innerText = movie.name;
+            moviesList.appendChild(li);
+        });
+
+    }).catch(function (err) {
+        console.log(err);
+        alert("Failed to fetch movies");
+    });
 }
